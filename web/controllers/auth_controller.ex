293f8f0ef624ee7d468048e3eebaf014a784d3aa
Spencer Markin <markinspencer@gmail.com>
2019-01-13 01:19:27 -0500

    implement sign in function that persists the user id in the browser

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -12,12 +12,25 @@ defmodule Discuss.AuthController do
       token: auth.credentials.token, 
       email: auth.info.email, 
       provider: provider 
-    }
-    
+    }  
     changeset = User.changeset(%User{}, user_params)
-    insert_or_update_user(changeset)
+  
+    sign_in(conn, changeset)
   end
 
+  defp sign_in(conn, changeset) do 
+    case insert_or_update_user(changeset) do 
+      {:ok, user} -> 
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} -> 
+        conn
+        |> put_flash(:error, "Error signing in!")
+        |> redirect(to: topic_path(conn, :index))   
+    end
+  end
 
   defp insert_or_update_user(changeset) do 
     case Repo.get_by(User, email: changeset.changes.email) do
